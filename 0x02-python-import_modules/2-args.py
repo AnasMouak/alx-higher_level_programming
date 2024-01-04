18d331ae2a2dfeff6ff7cc3e942c76a5152e5c60
AnasMouak <anasmouak1111@gmail.com>
2024-01-04 03:20:40 -0800

    Task_2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+
+    else:
+        if len(sys.argv) - 1 == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        
+        for c in range(1, len(sys.argv)):
+            print("{}: {}".format(c, sys.argv[c]))
