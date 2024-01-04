de086cfecdb3ccd254758380d1e1bc89b21baacc
AnasMouak <anasmouak1111@gmail.com>
2024-01-04 03:22:55 -0800

    Update_Task_2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,6 +11,5 @@ if __name__ == "__main__":
             print("1 argument:")
         else:
             print("{} arguments:".format(len(sys.argv) - 1))
-        
         for c in range(1, len(sys.argv)):
             print("{}: {}".format(c, sys.argv[c]))
