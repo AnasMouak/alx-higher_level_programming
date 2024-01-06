009eb57e9da3a488149283631d1015a053a200a6
AnasMouak <anasmouak1111@gmail.com>
2024-01-06 04:18:16 -0800

    Update_Task_4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     copy_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return copy_list
-    for i in copy_list:
+    for i, value in enumerate(copy_list):
         if i == idx:
             copy_list[i] = element
             return (copy_list)
