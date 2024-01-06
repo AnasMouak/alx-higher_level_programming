38154713a640b0884289bf28bca858b68fb5a63f
AnasMouak <anasmouak1111@gmail.com>
2024-01-06 03:00:44 -0800

    Update_Task_1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    for i in my_list:
+    for i, value in enumerate(my_list):
         if i == idx:
-            return (my_list[i])
+            return (value)
