a7b9556355171f95c00090bce83228a9fc76207b
AnasMouak <anasmouak1111@gmail.com>
2024-01-06 00:56:05 -0800

    Update_Task_6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{}".format(j), end=" ")
+            print("{:d}".format(j), end=" ")
         print()
