5dfbf95209315c5ba4948886554df95d74d16999
AnasMouak <anasmouak1111@gmail.com>
2024-01-22 06:48:40 -0800

    Update_Task_3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        return result
     except ZeroDivisionError:
         result = None
-        return result
     finally:
         print("Inside Result: {}".format(result))
+    return result
