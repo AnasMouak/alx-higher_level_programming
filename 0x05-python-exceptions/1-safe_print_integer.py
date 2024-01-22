6cf6cc24a3c2a306f674875feb203fa20a1e960b
AnasMouak <anasmouak1111@gmail.com>
2024-01-22 06:27:14 -0800

    Update_Task_1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value):
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except (ValueError, TypeError):
         return False
