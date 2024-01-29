b59ec25c7586d6b1c52c26f2801ab659fc64fb37
AnasMouak <anasmouak1111@gmail.com>
2024-01-29 06:10:41 -0800

    Task_0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """
+    This is a simple class representing a Rectangle.
+
+    Attributes:
+    - None
+
+    Methods:
+    - None
+    """
+    pass
