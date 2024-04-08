fc8e99b38065959090dbe627f5817cee1097ce1b
AnasMouak <anasmouak1111@gmail.com>
2024-04-08 15:00:28 -0700

    Argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length - 2 === 0) {
+  console.log('No argument');
+} else if (process.argv.length - 2 === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
