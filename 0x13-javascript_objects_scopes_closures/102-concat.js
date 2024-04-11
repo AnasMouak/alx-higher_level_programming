01260b2c2eda5aa80fc8ac58b53d818f3ccc66a2
AnasMouak <anasmouak1111@gmail.com>
2024-04-11 06:42:17 -0700

    concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file1Content = fs.readFileSync(process.argv[2], 'utf8');
+const file2Content = fs.readFileSync(process.argv[3], 'utf8');
+
+const combinedContent = file1Content + '\n' + file2Content;
+
+fs.writeFileSync(process.argv[4], combinedContent, 'utf8');
