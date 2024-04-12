d88aa242e93d1f59cea2f197e118c34d9d9945d9
AnasMouak <anasmouak1111@gmail.com>
2024-04-12 16:48:14 -0700

    State object with the name passed as Argv

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Starts a session with the database and queries states from the states table.
+It prints the ID of the first state whose name contains the specified string.
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    name_state = sys.argv[4]
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    states = session.query(State).all()
+
+    for state in states:
+        if name_state in state.name:
+            print(state.id)
+            break
+
+    else:
+        print("Not found")
+
+    session.close()
