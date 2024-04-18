cb6af5ed6cb2fc9da9c5bb529c856fcb140b8ab5
AnasMouak <anasmouak1111@gmail.com>
2024-04-18 05:12:05 -0700

    City relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Contains the class definition of a City
+"""
 from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
