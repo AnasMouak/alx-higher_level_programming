3dbecfbe20f1b8b4b6758ffbf0c7d2b8bb5d460b
AnasMouak <anasmouak1111@gmail.com>
2024-04-25 09:46:43 -0700

    Update cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | awk '/^Allow:/ {print $2,$3,$4}'
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
