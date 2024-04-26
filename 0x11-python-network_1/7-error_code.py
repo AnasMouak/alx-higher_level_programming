b19cee6c43d5416d2522339b350cc4bc0249eb69
AnasMouak <anasmouak1111@gmail.com>
2024-04-26 11:44:58 -0700

    requests Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''script that takes in a URL, sends a request to the URL and displays
+    the body of the response'''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    response = requests.get(sys.argv[1])
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(response.status_code)
