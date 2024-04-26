5fe0ddcda77edc43831ae8dc529f40365fdc6b72
AnasMouak <anasmouak1111@gmail.com>
2024-04-26 07:39:46 -0700

    fetches URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+'''script that fetches https://alx-intranet.hbtn.io/status'''
 
 import urllib.request
 
 if __name__ == "__main__":
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         print('Body response:')
         print('\t- type:', type(content))
